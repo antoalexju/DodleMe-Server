bc32e8cd38d21dc9c945973b0447839e418f91ae
root <root@lnxdodle1.local>
2020-05-14 03:28:37 +0200

    relation with front

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('../DodleMe-WebUI/dist/'));
+
+app.get('/', function (req, res) {
+    res.sendFile('index.html');
+});
+
+var server = app.listen(80, '10.0.0.37',  function () {
+    console.log('Node server is running..');
+});
