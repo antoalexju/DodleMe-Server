649b4f00b18c6303319555bfb99bfc3329150dfb
adonix-dev <antony.laget@icloud.com>
2020-05-27 00:46:21 +0200

    domain

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('../../DodleMe-WebUI/dist/'));
+app.use(express.static('/home/dodle/DodleMe-WebUI/dist/'));
 
 app.get('/', function (req, res) {
     res.sendFile('index.html');
