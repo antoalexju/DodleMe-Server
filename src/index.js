1e70412666f9c5736434e6e6461b3e7b7ffaf784
adonix-dev <antony.laget@icloud.com>
2020-05-27 05:38:54 +0200

    serv config

--- src/index.js
+++ src/index.js
@@ -1,40 +1,17 @@
 //Exemple: https://github.com/rwieruch/node-express-server-rest-api/blob/master/src/index.js
 
 var express = require('express');
-//var vhost = require('vhost')
+var app = express();
 var server = require('http').createServer(app)
 
-var app = express();
 app.use(express.static('../DodleMe-WebUI/dist/'));
+
 app.get('/', function (req, res) {
     res.sendFile('index.html', { root: '../DodleMe-WebUI/dist/' })
 });
-
-/*
-app.use(vhost('dodle.antonylaget.com', function handle (req, res, next) {
-    // for match of "foo.bar.example.com:8080" against "*.*.example.com":
-    console.dir(req.vhost.host) // => 'foo.bar.example.com:8080'
-    console.dir(req.vhost.hostname) // => 'foo.bar.example.com'
-    console.dir(req.vhost.length) // => 2
-    console.dir(req.vhost[0]) // => 'foo'
-    console.dir(req.vhost[1]) // => 'bar'
-}))
-
 //app.get( '/', routes.index );
 
-// an external api server in any framework
-var httpServer = http.createServer(function (req, res) {
-    res.setHeader('Content-Type', 'text/html')
-    res.sendFile('index.html', { root: '../DodleMe-WebUI/dist/' })
-})
-
-app.use(vhost('dodle.antonylaget.com', function (req, res) {
-    // handle req + res belonging to api.example.com
-    // pass the request to a standard Node.js HTTP server
-    httpServer.emit('request', req, res)
-}))
-*/
-
-server.listen(80, '10.0.0.37', function () {
+server.listen(80, '0.0.0.0', function () {
     console.log('Node server is running...');
 });
+
