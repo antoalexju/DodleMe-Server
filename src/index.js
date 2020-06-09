019deed74028326c58e6eff0ece1c9d774f4eda3
adonix-dev <antony.laget@icloud.com>
2020-06-09 04:02:40 +0200

    -- CORS patch --

--- src/index.js
+++ src/index.js
@@ -20,6 +20,11 @@ server.get('/app', function (req, res) {
     res.sendFile('index.html', { root: '../DodleMe-WebUI/dist/' })
 });
 
+server.use(function(req, res) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+});
+
 server.use('/api', apiRouter);
 
 
