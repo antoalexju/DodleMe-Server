25670ec491345f43afe056daa99a3187a7fe27e3
adonix-dev <antony.laget@icloud.com>
2020-06-09 04:17:16 +0200

    -- CORS patch 3 --

--- src/index.js
+++ src/index.js
@@ -14,17 +14,16 @@ server.use(bodyParser.json());
 server.set('json spaces', 4);
 const port = 80;
 
-server.get('/app', function (req, res) {
-    res.sendFile('index.html', { root: '../DodleMe-WebUI/dist/' })
-});
-
-server.use('/api', apiRouter);
-
 server.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 })
 
+server.get('/app', function (req, res) {
+    res.sendFile('index.html', { root: '../DodleMe-WebUI/dist/' })
+});
+
+server.use('/api', apiRouter);
 
 server.listen(port, '0.0.0.0', function () {
     console.log('Node server is running... (listening on port: ' + port + ')');
