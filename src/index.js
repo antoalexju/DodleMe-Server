ec209150eb7c630b0969520e0f3e1a75eadea027
adonix-dev <antony.laget@icloud.com>
2020-06-09 04:16:31 +0200

    -- CORS patch 2 --

--- src/index.js
+++ src/index.js
@@ -20,9 +20,9 @@ server.get('/app', function (req, res) {
 
 server.use('/api', apiRouter);
 
-server.options('/api', function (req, res, next) {
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-    res.send();
+server.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 })
 
 
