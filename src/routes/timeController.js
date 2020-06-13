99b6bbfc27a71c08639185b1adffab82e4581da2
JulietteLajoinie <lajoinie.juliette@gmail.com>
2020-06-13 12:59:05 +0200

    --add create in timeController.js--

--- src/routes/timeController.js
+++ src/routes/timeController.js
@@ -2,7 +2,24 @@ const rel = require('../database/models/relation');
 
 module.exports = {
     create: function (req, res) {
-
+        rel.Time.create({
+            beginDate: req.body.beginDate,
+            endDate: req.body.endDate,
+            idEventAttached: req.body.idEventAttached
+        })
+            .then(time => {
+                res.status(200).json({
+                    message: "Le nouveau créneau a bien été enregistré !",
+                    details: time,
+                    id: time.idTime
+                })
+            })
+            .catch(err => {
+                res.status(404).json({
+                    error: err + '',
+                    code: 404
+                });
+            });
     },
     changeTime: function (req, res) {
 
